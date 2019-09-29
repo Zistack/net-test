1bc57f7dbb3b81b86f1782f81c733a57a20ecec0
Zistack <Zistack@null.net>
2019-09-29 19:45:40 -0400

    Moved exception store and added additional excercise at the end

--- src/nursery-test/main.hpp
+++ src/nursery-test/main.hpp
@@ -2,40 +2,58 @@ int
 main (int argc, char ** argv)
 {
 	std::mutex output_mutex;
+	Failure::ExceptionStore::T exception_store;
 
-	Thread::Nursery::Aggregate::T nursery
-	(
-		std::forward_as_tuple
+	{
+		Thread::Nursery::Aggregate::T nursery
 		(
-			nullptr,
-			[&] ()
-			{
-				std::unique_lock output_lock (output_mutex);
+			exception_store,
+			std::forward_as_tuple
+			(
+				nullptr,
+				[&] ()
+				{
+					std::unique_lock output_lock (output_mutex);
 
-				printf ("0\n");
-			}
-		),
-		std::forward_as_tuple
+					printf ("0\n");
+				}
+			),
+			std::forward_as_tuple
+			(
+				nullptr,
+				[&] ()
+				{
+					std::unique_lock output_lock (output_mutex);
+
+					printf ("1\n");
+				}
+			),
+			std::forward_as_tuple
+			(
+				nullptr,
+				[&] ()
+				{
+					std::unique_lock output_lock (output_mutex);
+
+					printf ("2\n");
+				}
+			)
+		);
+	}
+
+	{
+		Thread::Nursery::Aggregate::T nursery
 		(
-			nullptr,
-			[&] ()
-			{
-				std::unique_lock output_lock (output_mutex);
+			exception_store,
+			std::forward_as_tuple
+			(
+				nullptr,
+				[] () { throw Failure::Error::T ("Bad\n"); }
+			)
+		);
+	}
 
-				printf ("1\n");
-			}
-		),
-		std::forward_as_tuple
-		(
-			nullptr,
-			[&] ()
-			{
-				std::unique_lock output_lock (output_mutex);
-
-				printf ("2\n");
-			}
-		)
-	);
+	exception_store . poll ();
 
 	return 0;
 }
