3766332044b975e6cb69d308859aaecfda4cb6e9
Zistack <Zistack@null.net>
2019-09-30 14:51:28 -0400

    Added output scope so that reponses would be properly flushed to the output stream

--- src/Echo/ProtocolProxy/Details/writeResponse.def.hpp
+++ src/Echo/ProtocolProxy/Details/writeResponse.def.hpp
@@ -2,5 +2,7 @@ template <typename OutputStream>
 void
 T::writeResponse (const std::string & response, OutputStream && output_stream)
 {
+	Scope::T output_scope (output_stream);
+
 	output_stream . print (response + "\n");
 }
