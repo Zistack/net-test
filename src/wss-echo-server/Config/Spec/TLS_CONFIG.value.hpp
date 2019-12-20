21b13d192924dc5efd9e6f227b72f3dc570d2b3d
Zistack <Zistack@null.net>
2019-12-20 14:24:56 -0500

    Fixed typos

--- src/wss-echo-server/Config/Spec/TLS_CONFIG.value.hpp
+++ src/wss-echo-server/Config/Spec/TLS_CONFIG.value.hpp
@@ -1,3 +1,3 @@
-static const std::string TLS_CONFIG = "TCP-Config";
+static const std::string TLS_CONFIG = "TLS-Config";
 static const std::string TLS_CONFIG_DESCRIPTION =
 	"Configuration for TLS server.";
